918a1c1b0ec030eb4df7cbb0444f14aa082a27a6
Mister Souza <mistersouza@icloud.com>
2024-04-04 12:31:51 +0200

    Write booking serializer

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -7,6 +7,11 @@ class MenuItemSerializer(serializers.ModelSerializer):
         model = MenuItem
         fields = '__all__'
         
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+        
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
