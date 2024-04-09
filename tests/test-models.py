f99263312161255b1277ec5cf5d0b41efee80dcb
Mister Souza <mistersouza@icloud.com>
2024-04-09 09:59:17 +0200

    Write MenuItem model unit test

--- tests/test-models.py
+++ tests/test-models.py
@@ -4,4 +4,4 @@ from restaurant.models import MenuItem
 class MenuItemTestCase(TestCase):
     def test_get_item(self):
         menuItem = MenuItem(dish='Burger', price=5.00, inventory=10)
-        self.assertEqual(menuItem.dish, 'Burger : 5.00')
+        self.assertEqual(str(menuItem), 'Burger : 5.00')
