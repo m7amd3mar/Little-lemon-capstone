1ebc9f0b4fa022fecee849f3201a593293d1ce03
Mister Souza <mistersouza@icloud.com>
2024-04-11 14:25:05 +0200

    Update app routes urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', MenuItemsView.as_view()),
-    path('<int:pk>', MenuItemDetailView.as_view()),
+    path('items', MenuItemsView.as_view()),
+    path('items/<int:pk>', MenuItemDetailView.as_view()),
 ]
